d172b3ceaef0e7abf8cc7cb7635fe68d16cee0d9
Chris Read <centurix@gmail.com>
2017-10-17 23:55:05 +1000

    Added more scheduling options plus prettier checkboxes

--- web/src/App.vue
+++ web/src/App.vue
@@ -19,7 +19,24 @@ export default {
 </script>
 
 <style lang="sass">
-@import "~bulma"
+@import "~bulma/sass/utilities/initial-variables.sass"
+
+$modal-content-width: 1000px
+
+@import "~bulma/sass/utilities/functions.sass"
+@import "~bulma/sass/utilities/derived-variables.sass"
+@import "~bulma/sass/utilities/animations.sass"
+@import "~bulma/sass/utilities/mixins.sass"
+@import "~bulma/sass/utilities/controls.sass"
+
+@import "~bulma/sass/base/_all"
+@import "~bulma/sass/elements/_all"
+@import "~bulma/sass/components/_all"
+@import "~bulma/sass/grid/_all"
+@import "~bulma/sass/layout/_all"
+
+@import "~cool-checkboxes-for-bulma.io/dist/css/bulma-radio-checkbox.min.css"
+
 @import "~bulma-badge/badge.sass"
 @import "~font-awesome/css/font-awesome.css"
 </style>
--- web/src/components/Job.vue
+++ web/src/components/Job.vue
@@ -9,168 +9,249 @@
           <button class="delete" aria-label="close" v-on:click="hide"></button>
         </header>
         <section class="modal-card-body">
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Name</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control has-icons-right">
-                  <input v-bind:class="{'input': true, 'is-danger': isValid('name')}" type="text" placeholder="Name" v-model="job.name" :disabled="editing">
-                  <span v-show="isValid('name')" class="icon is-small is-right">
-                    <i class="fa fa-warning"></i>
-                  </span>
+          <div class="columns">
+            <div class="column">
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">Name</label>
                 </div>
-                <p v-show="!isValid('name') && !editing" class="help">A name for this job is required</p>
-                <p v-show="isValid('name') && !editing" class="help is-danger">Invalid job name</p>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">From Host</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control has-icons-right">
-                  <div class="select" v-bind:class="{'select': true, 'is-fullwidth': true, 'is-danger': isValid('from_host')}" >
-                    <select v-model="job.from_host">
-                      <option disabled value="">Select a host</option>
-                      <option value="localhost">localhost</option>
-                      <option v-for="host in hosts">{{ host.attributes.host }}</option>
-                    </select>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control has-icons-right">
+                      <input v-bind:class="{'input': true, 'is-danger': isValid('name')}" type="text" placeholder="Name" v-model="job.name" :disabled="editing">
+                      <span v-show="isValid('name')" class="icon is-small is-right">
+                        <i class="fa fa-warning"></i>
+                      </span>
+                    </div>
+                    <p v-show="!isValid('name') && !editing" class="help">A name for this job is required</p>
+                    <p v-show="isValid('name') && !editing" class="help is-danger">Invalid job name</p>
                   </div>
                 </div>
-                <p v-show="!isValid('from_host')" class="help">A host for this job is required</p>
-                <p v-show="isValid('from_host')" class="help is-danger">Invalid host</p>
               </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">From Path</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control has-icons-right">
-                  <input v-bind:class="{'input': true, 'is-danger': isValid('from_path')}" type="text" placeholder="From Path" v-model="job.from_path">
-                  <span v-show="isValid('from_path')" class="icon is-small is-right">
-                    <i class="fa fa-warning"></i>
-                  </span>
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">From Host</label>
                 </div>
-                <p v-show="!isValid('from_path')" class="help">A path for this job is required</p>
-                <p v-show="isValid('from_path')" class="help is-danger">Invalid path</p>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">To Host</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control has-icons-right">
-                  <div class="select" v-bind:class="{'select': true, 'is-fullwidth': true, 'is-danger': isValid('to_host')}" >
-                    <select v-model="job.to_host">
-                      <option disabled value="">Select a host</option>
-                      <option value="localhost">localhost</option>
-                      <option v-for="host in hosts">{{ host.attributes.host }}</option>
-                    </select>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control has-icons-right">
+                      <div class="select" v-bind:class="{'select': true, 'is-fullwidth': true, 'is-danger': isValid('from_host')}" >
+                        <select v-model="job.from_host">
+                          <option disabled value="">Select a host</option>
+                          <option value="localhost">localhost</option>
+                          <option v-for="host in hosts">{{ host.attributes.host }}</option>
+                        </select>
+                      </div>
+                    </div>
+                    <p v-show="!isValid('from_host')" class="help">A host for this job is required</p>
+                    <p v-show="isValid('from_host')" class="help is-danger">Invalid host</p>
                   </div>
                 </div>
-                <p v-show="!isValid('to_host')" class="help">A host for this job is required</p>
-                <p v-show="isValid('to_host')" class="help is-danger">Invalid host</p>
               </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">To Path</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control has-icons-right">
-                  <input v-bind:class="{'input': true, 'is-danger': isValid('to_path')}" type="text" placeholder="To Path" v-model="job.to_path">
-                  <span v-show="isValid('to_path')" class="icon is-small is-right">
-                    <i class="fa fa-warning"></i>
-                  </span>
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">From Path</label>
                 </div>
-                <p v-show="!isValid('to_path')" class="help">A path for this job is required</p>
-                <p v-show="isValid('to_path')" class="help is-danger">Invalid path</p>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control has-icons-right">
+                      <input v-bind:class="{'input': true, 'is-danger': isValid('from_path')}" type="text" placeholder="From Path" v-model="job.from_path">
+                      <span v-show="isValid('from_path')" class="icon is-small is-right">
+                        <i class="fa fa-warning"></i>
+                      </span>
+                    </div>
+                    <p v-show="!isValid('from_path')" class="help">A path for this job is required</p>
+                    <p v-show="isValid('from_path')" class="help is-danger">Invalid path</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Repeat by</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <div class="select">
-                    <select v-model="schedule">
-                      <option value="">Not scheduled</option>
-                      <option>Second</option>
-                      <option>Minute</option>
-                      <option>Hour</option>
-                      <option>Day</option>
-                      <option>Week</option>
-                      <option>Month</option>
-                      <option>Year</option>
-                    </select>
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">To Host</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control has-icons-right">
+                      <div class="select" v-bind:class="{'select': true, 'is-fullwidth': true, 'is-danger': isValid('to_host')}" >
+                        <select v-model="job.to_host">
+                          <option disabled value="">Select a host</option>
+                          <option value="localhost">localhost</option>
+                          <option v-for="host in hosts">{{ host.attributes.host }}</option>
+                        </select>
+                      </div>
+                    </div>
+                    <p v-show="!isValid('to_host')" class="help">A host for this job is required</p>
+                    <p v-show="isValid('to_host')" class="help is-danger">Invalid host</p>
+                  </div>
+                </div>
+              </div>
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">To Path</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control has-icons-right">
+                      <input v-bind:class="{'input': true, 'is-danger': isValid('to_path')}" type="text" placeholder="To Path" v-model="job.to_path">
+                      <span v-show="isValid('to_path')" class="icon is-small is-right">
+                        <i class="fa fa-warning"></i>
+                      </span>
+                    </div>
+                    <p v-show="!isValid('to_path')" class="help">A path for this job is required</p>
+                    <p v-show="isValid('to_path')" class="help is-danger">Invalid path</p>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="field is-horizontal" v-show="schedule != ''">
-            <div class="field-label is-normal">
-              <label class="label">Every</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <input type="number" class="input" v-model="repeat">
+            <div class="column">
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">Repeat</label>
                 </div>
-                <p class="help">{{ schedule }}(s)</p>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <div class="select is-fullwidth">
+                        <select v-model="schedule">
+                          <option value="">Not scheduled</option>
+                          <option>Second</option>
+                          <option>Minute</option>
+                          <option>Hour</option>
+                          <option>Day</option>
+                          <option>Week</option>
+                          <option>Month</option>
+                          <option>Year</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="field is-horizontal" v-show="schedule == 'Day'">
-            <!-- Daily -->
-            <label class="label">Starts</label>&nbsp;
-            <div class="control">
-              <input type="text" class="input is-small">
-            </div>
-          </div>
-          <div class="field is-horizontal" v-show="schedule == 'Week'">
-            <!-- Weekly -->
-            <label class="label">Every</label>&nbsp;
-            <label class="checkbox"><input type="checkbox">&nbsp;M</label>
-            <label class="checkbox"><input type="checkbox">&nbsp;T</label>
-            <label class="checkbox"><input type="checkbox">&nbsp;W</label>
-            <label class="checkbox"><input type="checkbox">&nbsp;T</label>
-            <label class="checkbox"><input type="checkbox">&nbsp;F</label>
-            <label class="checkbox"><input type="checkbox">&nbsp;S</label>
-            <label class="checkbox"><input type="checkbox">&nbsp;S</label>
-          </div>
-          <div class="field is-horizontal" v-show="schedule == 'Month'">
-            <!-- Monthly -->
-            <label class="label">Run</label>&nbsp;
-            <div class="control">
-              <label class="radio">
-                <input type="radio" name="month_day">&nbsp;Day of the Month
-              </label>
-              <label class="radio">
-                <input type="radio" name="month_day">&nbsp;Day of the week
-              </label>
-            </div>
-          </div>
-          <div class="field is-horizontal" v-show="schedule == 'Year'">
-            <!-- Yearly -->
-            <label class="label">Day of the year</label>&nbsp;
-            <div class="control">
-              <input type="text" class="input">
+              <div class="field is-horizontal" v-show="schedule != ''">
+                <div class="field-label is-normal">
+                  <label class="label">Every</label>
+                </div>
+                <div class="field-body">
+                  <div class="field has-addons">
+                    <div class="control">
+                      <input type="number" class="input" v-model="repeat">
+                    </div>
+                    <div class="control">
+                      <span class="button is-primary">{{ schedule }}(s)</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="field is-horizontal" v-show="schedule == 'Day'">
+                <!-- Daily -->
+                <div class="field-label is-normal">
+                  <label class="label">Starts</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <input type="text" class="input">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="field is-horizontal" v-show="schedule == 'Week'">
+                <!-- Weekly -->
+                <div class="field-label is-normal">
+                  <label class="label">Every</label>
+                </div>
+                <div class="field-body">
+                  <ul>
+                    <li class="field">
+                      <div class="control">
+                        <div class="b-checkbox is-primary">
+                          <input type="checkbox" class="styled" id="monday">
+                          <label for="monday">Monday</label>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="field">
+                      <div class="control">
+                        <div class="b-checkbox is-primary">
+                          <input type="checkbox" class="styled" id="tuesday">
+                          <label for="tuesday">Tuesday</label>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="field">
+                      <div class="control">
+                        <div class="b-checkbox is-primary">
+                          <input type="checkbox" class="styled" id="wednesday">
+                          <label for="wednesday">Wednesday</label>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="field">
+                      <div class="control">
+                        <div class="b-checkbox is-primary">
+                          <input type="checkbox" class="styled" id="thursday">
+                          <label for="thursday">Thursday</label>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="field">
+                      <div class="control">
+                        <div class="b-checkbox is-primary">
+                          <input type="checkbox" class="styled" id="friday">
+                          <label for="friday">Friday</label>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="field">
+                      <div class="control">
+                        <div class="b-checkbox is-primary">
+                          <input type="checkbox" class="styled" id="saturday">
+                          <label for="saturday">Saturday</label>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="field">
+                      <div class="control">
+                        <div class="b-checkbox is-primary">
+                          <input type="checkbox" class="styled" id="sunday">
+                          <label for="sunday">Sunday</label>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="field is-horizontal" v-show="schedule == 'Month'">
+                <!-- Monthly -->
+                <div class="field-label is-normal">
+                  <label class="label">Run</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <div class="select is-fullwidth">
+                        <select>
+                          <option>Day of the Month</option>
+                          <option>Day of the Week</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="field is-horizontal" v-show="schedule == 'Year'">
+                <!-- Yearly -->
+                <div class="field-label is-normal">
+                  <label class="label">Day of the year</label>&nbsp;
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                      <input type="text" class="input">
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
